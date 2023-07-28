58db28b68434937c858aa7f851def2fe4c9b52a7
Emma Hataway <emma.hataway@dri.edu>
2023-07-28 13:47:46 -0700

    norm

--- src/main.py
+++ src/main.py
@@ -160,8 +160,8 @@ def main(args):
         class_names = [c["name"] for c in cats]
 
         #ADDED
-        #class_names = [class_names[0], 'filler1', class_names[1], 'filler2', class_names[2]]
-        class_names = [class_names[0]]
+        class_names = [class_names[0], 'filler1', class_names[1], 'filler2', class_names[2]]
+        # class_names = [class_names[0]]
 
     model = load_model(args, coco_groundtruth, class_names=class_names)
     trainer = load_trainer(args)
